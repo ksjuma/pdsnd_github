6b5c4bb81a4200633bc3f4aa6e6f7ca6cbe0f830
Kayiwa Shafik Juma <ksjuma09@gmail.com>
2021-09-25 15:49:36 +0300

    Changed show_data function to show_more_data

--- bikeshare.py
+++ bikeshare.py
@@ -221,7 +221,7 @@ def user_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
     
-def show_data(df):
+def show_more_data(df):
     # Show more rows of data upon user request.   
     
         view_data = input("Would you like to view 5 rows of individual trip data? Enter yes or no?")
@@ -249,7 +249,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-        show_data(df)
+        show_more_data(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
